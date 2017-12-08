344c05e9a0eea4d1a01f7b7b7e954d8251c29ab8
Vitor Rocha <vitor.vrs@gmail.com>
2017-12-07 23:55:51 -0200

    Transformado dados de string hex para string ascii

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
+use std::str;
+
 fn main() {
 
   let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string();
-  // let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string();
+  // let output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
-  hex2bin(input);
+  println!("{:?}", hex2bin(input));
 }
 
-fn hex2bin(input: String) {
+fn hex2bin(input: String) -> String {
 
   let bytes: Vec<char> = input.chars().collect();
-  let mut result: Vec<u32> = Vec::new();
+  let mut result: Vec<u8> = Vec::new();
 
   for chunk in bytes.chunks(2) {
     
-    println!("0x{:?}{:?}", chunk[0], chunk[1]);
-
     // arrumar erro de conversao de letra para numero
-    let first: u32 = chunk[0].to_string().parse().unwrap();
-    let second: u32 = chunk[1].to_string().parse().unwrap();
+    let first = chunk[0].to_digit(16).unwrap();
+    let second = chunk[1].to_digit(16).unwrap();
     
+    let number = ((first << 4) | second) as u8;
+    result.push(number);
   }
 
-  // println!("{:?}", result);
-  // input
+  // variavel result contém os bytes das letras
+
+  String::from_utf8(result).unwrap()
 }
 
 // todo
-// converter a string em hexadecimal para bytes
+// converter a string em hexadecimal para bytes - feito
 // converter os bytes para base 64
 
 // I'm
